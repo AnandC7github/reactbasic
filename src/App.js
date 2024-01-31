d105c0499ef48c73962a0b691d9c987bb01b32d3
Anand.C <anandchannazhi01@gmail.com>
2024-01-31 13:00:29 +0530

    added h2 tage ..wrapped both h1 and h2 inside <> & </>

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
 
 const App = () => {
-  const name = 'John';
+  const name = null;
   const isNameShowing = false;
   return (
     <div className="App">
@@ -11,7 +11,10 @@ const App = () => {
         test
       </>
       ) : (
-        <h1>test</h1>
+        <>
+          <h1>test</h1>
+          <h2>There is no name.</h2>
+        </>
       )}
 
     </div>
