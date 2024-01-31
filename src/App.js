bb6137f07e1f9575eb3b20cdbbeb661839d595f5
Anand.C <anandchannazhi01@gmail.com>
2024-01-31 12:51:25 +0530

    changed the true value to false

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 
 const App = () => {
   const name = 'John';
-  const isNameShowing = true;
+  const isNameShowing = false;
   return (
     <div className="App">
       <h1>Hello, {isNameShowing ? name : 'someone'}!</h1>
