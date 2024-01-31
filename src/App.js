4dff15f2718e63ee2c9ab676b032850b488093c3
Anand.C <anandchannazhi01@gmail.com>
2024-01-31 12:43:01 +0530

    injected a js code

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
 
 const App = () => {
+  const name = 'John';
   return (
     <div className="App">
-      <h1>Hello, JSM!</h1>
+      <h1>Hello, {name}!</h1>
+
     </div>
   );
 }
