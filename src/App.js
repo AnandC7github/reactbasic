71945bb4505187c30eda4e38252675159f8c5f63
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 12:05:24 +0530

    now with the API, we are getting a response for the search of the title 'Spiderman' when we check in inspect part.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,22 @@
 import React from "react";
+import { useEffect } from "react";
 
 
 //Created an API URL
 const API_URL = 'https://www.omdbapi.com?apikey=4d067738';
 const App = () => {
+
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data);
+    }
+    //useeffect accepts a callback function and a dependency array as the other one if we want to call it from start only
+    useEffect(() => {
+        searchMovies('Spiderman');
+
+    }, []);
     return (
         <h1>App</h1>
     );
