2dfbd848484452f37ac03a0202211bfe3a1f5102
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 14:14:42 +0530

    added a placeholder if the movie image is not available.

--- src/App.js
+++ src/App.js
@@ -49,7 +49,7 @@ const App = () => {
                         <p>{movie1.Year}</p>
                     </div>
                     <div>
-                        <img src={movie1.Poster} alt="movie1.Title"/>
+                        <img src={movie1.Poster !== 'N/A' ? movie1.Poster :  'https://via.placeholder.com/400'} alt="movie1.Title"/>
                     </div>
                 </div>
 
