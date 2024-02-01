742b180fa95101a0d1fcc90efde88d67965e2bbe
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 10:28:47 +0530

    added useEffect and its property

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useState , useEffect} from 'react';
 import './App.css';
 
 const App = () => {
@@ -6,6 +6,12 @@ const App = () => {
   // Name and setter function for 1st variable
   // inside use state we provide the initial value
   const [counter, setCounter] = useState(0);
+
+  //A callback function
+  // As soon as the above component renders, this one gives alert
+  useEffect(() => {
+    alert('Reload')
+  })
   return (
     <div className="App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
