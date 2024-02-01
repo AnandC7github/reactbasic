d31826388ff487a19ff0bfd5ef33e1b4e92b896b
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 10:36:55 +0530

    setter function has been added to set the counter to 100 while reloading.

--- src/App.js
+++ src/App.js
@@ -7,12 +7,12 @@ const App = () => {
   // inside use state we provide the initial value
   const [counter, setCounter] = useState(0);
 
-  //A callback function
-  // As soon as the above component renders, this one gives alert
-  useEffect(() => {
-    // we can never modify state manually. So it will be an error.
-    counter = 100;
-  })
+    //A callback function
+    // As soon as the above component renders, this one gives alert
+    useEffect(() => {
+      // This is right way as we can set counter to 100 while reload using only this setter function
+      setCounter(100);
+    }, [setCounter]);
   return (
     <div className="App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
