ba708d80b51db7ae2860ec4e04d2800ade76ee3f
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 10:20:53 +0530

    A small error - didnt initialize the State..

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ const App = () => {
   //Const state - hook
   // Name and setter function for 1st variable
   // inside use state we provide the initial value
-  const [counter, setCounter] = useState();
+  const [counter, setCounter] = useState(0);
   return (
     <div className="App">
       <button onClick={() => setCounter((prevCount) => prevCount - 1)}>-</button>
