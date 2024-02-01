cc233a7ad3c65245a0dbe08ba5d85e2572364082
Anand.C <anandchannazhi01@gmail.com>
2024-02-01 10:11:49 +0530

    given an alert message for button onclick.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,16 @@
+import { useState } from 'react';
 import './App.css';
 
-//new functional Component
-const Person = (props) => {
-  return(
-  <>
-   <h1>First Name : {props.name}</h1>
-   <h2>Last Name : {props.lastName}</h2>
-   <h2>Age : {props.Age}</h2>
-  </>
-)}
-
 const App = () => {
+  //Const state - hook
+  // Name and setter function for 1st variable
+  // inside use state we provide the initial value
+  const [counter, setCounter] = useState();
   return (
     <div className="App">
-      < Person name = {'John'} lastName = {'Doe'} Age = {30}/>
-      < Person name = {'Kyle'} lastName = {'Mayers'} Age = {32}/>
-      < Person name = {'Manpreet'} lastName = {'Singh'} Age = {40}/>
-      < Person name = {'Jaden'} lastName = {'Dernbach'} Age = {32}/>
-      < Person name = {'Rahul'} lastName = {'Sharma'} Age = {45}/>
+      <button onClick={() => alert('clicked')}>-</button>
+      <h1>{counter}</h1>
+      <button>+</button>
     </div>
   );
 }
